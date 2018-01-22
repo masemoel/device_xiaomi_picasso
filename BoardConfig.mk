a3f2c55fab3670a4be15298fac75193140630f3d
Kumar Anand <kumaranand@google.com>
2018-01-22 15:02:54 -0800

    picasso: Enable automatic channel selection.
     * Automatic Channel Selection algorithms and implementations
       are used to enable interfaces to automatically figure
       out which channel configuration to use for initiating
       communication, for any mode of operation which initiates
       radiation (AP, Mesh, IBSS, P2P).
    
       https://wireless.wiki.kernel.org/en/users/documentation/acs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -211,5 +211,7 @@ WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
+CONFIG_ACS := true
+
 # Inherit from the proprietary version
 include vendor/xiaomi/picasso/BoardConfigVendor.mk
