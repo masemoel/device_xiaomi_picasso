a7fdbe73c633a5bbda8b36f1d0eef22ea54297bd
Pig <pig.priv@gmail.com>
2020-07-20 01:31:40 +0200

    picasso: Reworked init
     * Slightly based on OnePlus3.
    
    Co-authored-by: dianlujitao <dianlujitao@lineageos.org>
    Change-Id: I6b9b320db2eec82da3d38ecb887634b6451abaf5

--- init/init_picasso.cpp
+++ init/init_picasso.cpp
@@ -2,7 +2,6 @@
    Copyright (c) 2015, The Linux Foundation. All rights reserved.
    Copyright (C) 2016 The CyanogenMod Project.
    Copyright (C) 2019-2020 The LineageOS Project.
-
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
    met:
@@ -34,108 +33,99 @@
 
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <sys/_system_properties.h>
 
 #include "property_service.h"
 #include "vendor_init.h"
 
 using android::base::GetProperty;
+using android::init::property_set;
 
-std::vector<std::string> ro_props_default_source_order = {
-    "",
-    "build.",
-    "bootimage.",
-    "odm.",
-    "product.",
-    "system.",
-    "system_ext.",
-    "vendor.",
+constexpr const char *RO_PROP_SOURCES[] = {
+    nullptr,   "product.", "product_services.", "odm.",
+    "vendor.", "system.", "system_ext.", "bootimage.",
+};
+
+constexpr const char *PRODUCTS[] = {
+    "picasso",
+    "picasso_48m",
+};
+
+constexpr const char *DEVICES[] = {
+    "Redmi K30 5G",
+    "Redmi K30i 5G",
+};
+
+constexpr const char *BUILD_DESCRIPTION[] = {
+    "coral-user 11 RQ1A.210105.003 7005429 release-keys",
+};
+
+constexpr const char *BUILD_FINGERPRINT[] = {
+    "google/coral/coral:11/RQ1A.210105.003/7005429:user/"
+    "release-keys",
+};
+
+constexpr const char *CLIENT_ID[] = {
+    "android-xiaomi",
+    "android-xiaomi-rev1",
 };
 
 void property_override(char const prop[], char const value[], bool add = true) {
-    prop_info *pi;
+  prop_info *pi;
 
-    pi = (prop_info *)__system_property_find(prop);
-    if (pi)
-        __system_property_update(pi, value, strlen(value));
-    else if (add)
-        __system_property_add(prop, strlen(prop), value, strlen(value));
+  pi = (prop_info *)__system_property_find(prop);
+  if (pi)
+    __system_property_update(pi, value, strlen(value));
+  else if (add)
+    __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-	/* From Magisk@jni/magiskhide/hide_utils.c */
-static const char *snet_prop_key[] = {
-    "ro.boot.vbmeta.device_state",
-    "ro.boot.verifiedbootstate",
-    "ro.boot.flash.locked",
-    "ro.boot.selinux",
-    "ro.boot.veritymode",
-    "ro.boot.warranty_bit",
-    "ro.warranty_bit",
-    "ro.debuggable",
-    "ro.secure",
-    "ro.build.type",
-    "ro.build.tags",
-    "ro.build.selinux",
-    NULL
-};
+void load_props(const char *model, bool is_or = false, bool is_48 = false) {
+  const auto ro_prop_override = [](const char *source, const char *prop,
+                                   const char *value, bool product) {
+    std::string prop_name = "ro.";
 
- static const char *snet_prop_value[] = {
-    "locked",
-    "green",
-    "1",
-    "enforcing",
-    "enforcing",
-    "0",
-    "0",
-    "0",
-    "1",
-    "user",
-    "release-keys",
-    "1",
-    NULL
-};
+    if (product)
+      prop_name += "product.";
+    if (source != nullptr)
+      prop_name += source;
+    if (!product)
+      prop_name += "build.";
+    prop_name += prop;
 
- static void workaround_snet_properties() {
+    property_override(prop_name.c_str(), value);
+  };
 
-     // Hide all sensitive props
-    for (int i = 0; snet_prop_key[i]; ++i) {
-        property_override(snet_prop_key[i], snet_prop_value[i]);
+  for (const auto &source : RO_PROP_SOURCES) {
+    ro_prop_override(source, "device", is_48 ? PRODUCTS[1] : PRODUCTS[0], true);
+    ro_prop_override(source, "model", model, true);
+    ro_prop_override(source, "fingerprint", BUILD_FINGERPRINT[0], false);
+    ro_prop_override(nullptr, "fingerprint", BUILD_FINGERPRINT[0], false);
+    ro_prop_override(nullptr, "description", BUILD_DESCRIPTION[0], false);
+    if (!is_48) {
+      ro_prop_override(source, "name", PRODUCTS[0], true);
+    } else {
+      ro_prop_override(source, "name", PRODUCTS[1], true);
     }
+  }
+  ro_prop_override(nullptr, "description", BUILD_DESCRIPTION[0], false);
+  ro_prop_override(nullptr, "product", model, false);
+  ro_prop_override(nullptr, "com.google.clientidbase", CLIENT_ID[0], false);
+  ro_prop_override(nullptr, "com.google.clientidbase.ms", CLIENT_ID[1], false);
+  property_override("ro.oem_unlock_supported", "0");
+  property_override("ro.boot.verifiedbootstate", "green");
 
      chmod("/sys/fs/selinux/enforce", 0640);
     chmod("/sys/fs/selinux/policy", 0440);
 }
 
-void property_override_multifp(char const buildfp[], char const systemfp[],
-	char const bootimagefp[], char const vendorfp[], char const odmfp[], char const value[])
-{
-	property_override(buildfp, value);
-	property_override(systemfp, value);
-	property_override(bootimagefp, value);
-	property_override(vendorfp, value);
-    property_override(odmfp, value);
-}
-
 void vendor_load_properties() {
-    const auto set_ro_product_prop = [](const std::string &source,
-                                        const std::string &prop,
-                                        const std::string &value) {
-        auto prop_name = "ro.product." + source + prop;
-        property_override(prop_name.c_str(), value.c_str(), false);
-    };
+  std::string variant;
+  variant = GetProperty("ro.product.vendor.name", "");
 
-    for (const auto &source : ro_props_default_source_order) {
-        set_ro_product_prop(source, "brand", "Redmi");
-        set_ro_product_prop(source, "device", "picasso");
-        set_ro_product_prop(source, "model", "Redmi K30 5G");
-    }
-    property_override("ro.build.description", "coral-user 11 RP1A.201105.002 6869500 release-keys");
-    property_override("ro.oem_unlock_supported", "0");
-    property_override_multifp("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.bootimage.build.fingerprint",
-	    "ro.vendor.build.fingerprint", "ro.odm.build.fingerprint", "google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys");
-
-     // Workaround SafetyNet
-    workaround_snet_properties();
+  if (variant == "picasso") {
+    load_props(DEVICES[0], true, false);
+  } else if (variant == "picasso_48m") {
+    load_props(DEVICES[1], false, true);
+  }
 }
