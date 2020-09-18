317e840499af6ee42862849524532e0cca950851
Pig <pig.priv@gmail.com>
2020-09-19 00:09:28 +0800

    picasso: light: Extend hidl changes to aidl
     * Changes authored by dianlujitao in commits 7174356 ("Improve lights HAL") and
     f8e30fd ("Fix battery light not lit after notification light off") are
     adapted as following:
     Type(HIDL) -> id(AIDL) in functions and maps
    
     Note: Keep backlight in the map as per https://github.com/LineageOS/android_frameworks_base/commit/b9e44e5007c57e6f0b148385a21a800a51a7f8e5, allowing SF to set backlight.
    
    Co-authored-by: dianlujitao <dianlujitao@lineageos.org>
    Change-Id: I8960a1fe0b44097e703f3f067d4e432a12d26fc3

--- device.mk
+++ device.mk
@@ -295,6 +295,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.lights-service.xiaomi_picasso
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
