7bd2dab8122f9a0856f730d5fdc5de8b0e7dcec3
masemoel <az.az.az.mjme@gmail.com>
2022-04-23 18:37:01 +0200

    picasso: Build HWUI for performance

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,6 +100,9 @@ BOARD_HAVE_QCOM_FM := true
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_picasso
 TARGET_RECOVERY_DEVICE_MODULES := libinit_picasso
