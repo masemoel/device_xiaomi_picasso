678550b2ba383e87b0b2577cb763c7b774b8ebe4
masemoel <az.az.az.mjme@gmail.com>
2022-06-29 21:48:59 +0200

    picasso: Update blobs to MIUI CN stable 13.0.2
    - Pinned fingerprint, widevine and 48m camera blobs.
    
    Signed-off-by: masemoel <az.az.az.mjme@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -179,7 +179,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-12-01
+VENDOR_SECURITY_PATCH := 2022-05-01
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
--- update-sha1sums.py
+++ update-sha1sums.py
@@ -20,7 +20,7 @@ import os
 import sys
 from hashlib import sha1
 
-device = 'miatoll'
+device = 'picasso'
 vendor = 'xiaomi'
 
 with open('proprietary-files.txt', 'r') as f:
