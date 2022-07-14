6fe74a8abe493848b91591117444866cc4990a25
masemoel <az.az.az.mjme@gmail.com>
2022-07-14 22:36:48 +0200

    picasso: Ignore SELinux neverallows [TMP]

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -183,7 +183,7 @@ VENDOR_SECURITY_PATCH := 2022-05-01
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
-
+SELINUX_IGNORE_NEVERALLOWS := true
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
