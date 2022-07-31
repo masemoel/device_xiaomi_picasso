f592fdde87b02cd497974f78486548b02cae3d0a
masemoel <az.az.az.mjme@gmail.com>
2022-07-31 11:30:22 +0200

    picasso: Address vendor_sysfs_battery_supply denials properly
    Signed-off-by: masemoel <az.az.az.mjme@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -189,6 +189,9 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
+BOARD_SEPOLICY_M4DEFS += \
+    sysfs_battery_supply=vendor_sysfs_battery_supply
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
