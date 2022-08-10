d048bd1b511651dfa890c397c476851a86f31d93
LuK1337 <priv.luk@gmail.com>
2022-08-10 10:18:35 -0700

    picasso: Build the ANT+ service
    Change-Id: Ief5b8e47abf7a5daf8562ac0905248b9418740a2

--- device.mk
+++ device.mk
@@ -99,6 +99,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService-Soong \
+    com.dsi.ant@1.0.vendor
+
 # Atrace
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
