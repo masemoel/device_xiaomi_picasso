1bcc6a67938159a3d359dc753a2e76d70fd8877c
tarsin <yuanqingxiang233@163.com>
2022-08-18 21:54:00 +0800

    picasso: Add shim for missing symbol in com.mi.node.watermark.so

--- device.mk
+++ device.mk
@@ -501,6 +501,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
+# Shims
+PRODUCT_PACKAGES += \
+    lib-watermarkshim
+
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
