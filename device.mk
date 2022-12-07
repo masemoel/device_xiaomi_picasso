6bc7710616af0e089ca3cb1e6c88dc725a99f850
masemoel <az.az.az.mjme@gmail.com>
2022-12-07 17:42:08 +0100

    Revert "picasso: Migrate to Xiaomi power AIDL HAL"
    This reverts commit 41fb234ebf3aa01efa23e4b8b1e2995b91b6d1b1. Throws a building error on A13.

--- device.mk
+++ device.mk
@@ -451,14 +451,10 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
-
-PRODUCT_SOONG_NAMESPACES += \
-    hardware/google/interfaces \
-    hardware/google/pixel
 
 # QMI
 PRODUCT_PACKAGES += \
