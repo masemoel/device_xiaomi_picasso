1d90646b57219f0786a3b3f4884fd48d9429782d
masemoel <az.az.az.mjme@gmail.com>
2022-12-17 12:34:34 +0100

    Revert "picasso: Add Google Camera Go"
    In favour of Aperture.
    This reverts commit 08c7dc8c734ef79c638212d94842a163f6a32c9f.

--- device.mk
+++ device.mk
@@ -181,10 +181,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    Aperture \
     libcamera2ndk_vendor \
     libdng_sdk.vendor \
     libgui_vendor \
-    GCamGO \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
