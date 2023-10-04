a4a65381e5393b62ddb6afb388510567895739d8
Manuel José M <az.az.az.mjme@gmail.com>
2023-10-04 23:37:45 +0200

    Revert "picasso: Drop PocketMode [TMP]"
    This reverts commit 64a9d3adc9abf254edaf7d7ea6b66ee01df15bbb.

--- device.mk
+++ device.mk
@@ -454,6 +454,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2.vendor
 
+# PocketMode
+PRODUCT_PACKAGES += \
+    PocketMode
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/pocket/privapp-permissions-pocketmode.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-pocketmode.xml
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
