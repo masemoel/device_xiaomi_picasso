a1b750a1646eacae8ad43e17263c48a0cdd53d2a
Sarthak Roy <sarthakroy2002@gmail.com>
2023-10-30 17:46:18 +0100

    picasso: Switch to Wifi service AIDL

--- device.mk
+++ device.mk
@@ -566,7 +566,7 @@ PRODUCT_PACKAGES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     android.hardware.wifi.hostapd@1.0.vendor \
     hostapd \
     libwifi-hal-ctrl \
